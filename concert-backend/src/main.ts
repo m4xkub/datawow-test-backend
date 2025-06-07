f6f31000ceb4cb965cb2c8d5737fee7466929665
max <153589599+m4xkub@users.noreply.github.com>
2025-06-07 16:48:04 +0700

    Update main.ts

--- concert-backend/src/main.ts
+++ concert-backend/src/main.ts
@@ -5,7 +5,6 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-
   const config = new DocumentBuilder()
     .setTitle('Concert Reservation API')
     .setDescription('API for managing concert tickets')
@@ -16,10 +15,10 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:3000',
     credentials: true,
   });
 
-  await app.listen(3000);
+  await app.listen(8000);
 }
 bootstrap();
